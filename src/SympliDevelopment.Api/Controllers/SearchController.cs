15c0b7bdfa93db49b34a58c4ef6c1048237ccc4b
Rui Cheng <rui.cheng@sympil.com.au>
2023-03-09 13:16:48 +1100

    clean up csproj and SearchController

--- src/SympliDevelopment.Api/Controllers/SearchController.cs
+++ src/SympliDevelopment.Api/Controllers/SearchController.cs
@@ -2,18 +2,19 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SympliDevelopment.Api.Controllers
 {
-  [ApiController]
-  [Route("[controller]")]
-  public class SearchController : ControllerBase
-  {
-   
-    [HttpGet("keywords")]
-    public async Task<IActionResult> GetResult([FromQuery] string keywords)
+    [ApiController]
+    [Route("[controller]")]
+    public class SearchController : ControllerBase
     {
-        // please implement this method to return the result correctly.
-        // the method receives an input keywords and then return the ranking result
-        
+        [HttpGet("keywords")]
+        public Task<IActionResult> GetResult([FromQuery] string keywords)
+        {
+            // please implement this method to return the result correctly.
+            // the method receives an input keywords and then return the ranking result
+
+            // NOTE that you're free to change the return type from Task<IActionResult>
+            // to anything else; you can also change the function parameters if needed.
+            throw new NotImplementedException();
+        }
     }
-    
-  }
 }
